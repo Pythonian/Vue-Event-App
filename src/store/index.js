043e63dbacff6c7b6e82bb171f1da952cb790d24
Seyi Pythonian <prontomaster@gmail.com>
2020-04-16 17:47:08 +0100

    Cached EventShow component with vuex

--- src/store/index.js
+++ src/store/index.js
@@ -17,7 +17,8 @@ export default new Vuex.Store({
       'community'
     ],
     events: [],
-    eventsTotal: 0
+    eventsTotal: 0,
+    event: {}
   },
   mutations: {
     ADD_EVENT(state, event) {
@@ -28,6 +29,9 @@ export default new Vuex.Store({
     },
     SET_EVENTS_TOTAL(state, eventsTotal) {
       state.eventsTotal = eventsTotal
+    },
+    SET_EVENT(state, event) {
+      state.event = event
     }
   },
   actions: {
@@ -48,6 +52,15 @@ export default new Vuex.Store({
         })
         .catch(error => {
           console.log('There was an error:' + error.response)
+        })
+    },
+    fetchEvent({ commit }, id) {
+      EventService.getEvent(id)
+        .then(response => {
+          commit('SET_EVENT', response.data)
+        })
+        .catch(error => {
+          console.log('There was an error:', error.response)
         })
     }
   },
